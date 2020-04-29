3ec103dd3dc372fc6619ec9fd4b96924f76cc554
christophe_v <cv816@ic.ac.uk>
2020-04-29 11:57:17 +0100

    no message

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.getcwd() + '\..\..\src')
+#sys.path.insert(0, os.getcwd() + '\..\..\src')
+sys.path.insert(0, os.path.abspath('..\..\src'))
 
 
 # -- Project information -----------------------------------------------------
