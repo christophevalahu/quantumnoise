d2721a3de6b668b2a8af7182907d6135f0781f8a
christophe_v <cv816@ic.ac.uk>
2020-04-29 13:51:48 +0100

    no message

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,12 +15,6 @@ import sys
 sys.path.insert(0, os.path.abspath('../../src/'))
 sys.path.insert(0, os.path.abspath('/src/'))
 
-# -- MOCK MODULES ------------
-import mock
-
-MOCK_MODULE = ['Cython']
-for mod_name in MOCK_MODULE :
-    sys.modules[mod_name] = mock.Mock()
 
 autodoc_mock_imports = ["qutip"]
 
@@ -40,7 +34,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
+"sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
