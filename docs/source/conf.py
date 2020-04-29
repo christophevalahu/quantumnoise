4627f9c03dbfe9205c42ada675a1cfb882263772
christophe_v <cv816@ic.ac.uk>
2020-04-29 14:08:32 +0100

    no message

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,8 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
 "sphinx_rtd_theme"
 ]
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
